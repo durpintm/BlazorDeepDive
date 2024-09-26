4df0621fe47ea355c253555926f9247e1ca92bda
durpintm <durpinthapa@gmail.com>
2024-09-26 14:06:10 -0400

    Adding card with images

---
+++ ServerManagement/Models/CitiesRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ServerManagement.Models
+{
+    public static class CitiesRepository
+    {
+        private static List<string> Cities = new List<string> { "Toronto", "Calgary", "Halifax", "London" };
+
+        public static List<string> GetCities() => Cities;
+    }
+}
